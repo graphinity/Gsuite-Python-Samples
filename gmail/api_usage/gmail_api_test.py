c314d3870c63642adc8bd89ea5982535c76d080c
Graphinity <graphinitytech@gmail.com>
2019-02-14 01:33:37 -0500

    Changes made to message content

--- gmail/api_usage/gmail_api_test.py
+++ gmail/api_usage/gmail_api_test.py
@@ -1,11 +1,13 @@
 # Test Gmail API methods
 
 from gmail.api_usage.gmail_credentials import credentials
-from gmail.api_usage.gmail_messages import show_message_threads
+from gmail.api_usage.gmail_messages_subject import show_message_threads
+from gmail.api_usage.gmail_messages_content import show_message_content
 from gmail.api_usage.gmail_labels import show_labels_threads
 
 if __name__ == '__main__':
     service = credentials()
-    show_labels_threads(service=service)
-    show_message_threads(service=service)
+    # show_labels_threads(service=service)
+    # show_message_threads(service=service)
+    show_message_content(service=service)
 
--- gmail/api_usage/gmail_credentials.py
+++ gmail/api_usage/gmail_credentials.py
@@ -30,7 +30,7 @@ def credentials():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials-gmail-priya.json', SCOPES)
+                'credentials-gmail-kshitij.json', SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
         with open('token.pickle', 'wb') as token:
